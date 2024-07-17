eb31bc88351f48c727c4a3ca7658a24847d16d05
vivek <iamvivekstyles@gmail.com>
2024-07-17 16:43:02 +0530

    changing script 1.8

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -13,47 +13,20 @@ log_message() {
   echo "$(date -u): $1"
 }
 
-# log_message "Node.js version: $(node --version)"
-# log_message "npm version: $(npm --version)"
-# log_message "started.............."
+log_message "Node.js version: $(node --version)"
+log_message "npm version: $(npm --version)"
+log_message "started.............."
 
-# # # Install system dependencies for sharp
-# sudo apt-get update
-# # sudo apt-get install -y libvips-dev
 
-# # # Navigate to the application directory
+# Navigate to the application directory
 cd /var/www/html || { log_message "Failed to change directory"; exit 1; }
 
-# # # Remove node_modules and reinstall
-# rm -rf node_modules package-lock.json
 
 # Clear npm cache
 npm cache clean --force
 
-# # # Reset npm configuration
-# # npm config delete prefix
-# # npm config set prefix "${HOME}/.npm-global"
-
-# # # Create user-specific global npm directory
-# # mkdir -p "${HOME}/.npm-global"
-
-# # # Add the new path to system PATH
-# # echo 'export PATH=$HOME/.npm-global/bin:$PATH' >> ~/.bashrc
-# # source ~/.bashrc
-
-# # # Install dependencies
+# Install dependencies
 sudo npm install --unsafe-perm
-
-# # log_message "Ratoons API Going To Start..............!!!!!!!!"
-
-# # # Install nodemon globally for the current user
-# # npm install -g nodemon
-
-# # # Install node-libcurl separately
-# # npm install node-libcurl --unsafe-perm
-
-# # log_message 'End---------->'
-# npm install -g nodemon >> "$INSTALL_LOG" 2>&1
 
 log_message "Checking for processes on port 1533..."
 PORT_PID=$(sudo lsof -t -i:1533)
@@ -67,5 +40,5 @@ fi
 
 # Run the application
 log_message 'Starting the application...'
-nohup npx --yes nodemon ratoons.js & >> "$INSTALL_LOG" 2>&1
+nohup npx --yes nodemon ratoons.js >> "$INSTALL_LOG" 2>&1 &
 log_message 'Ratoons Successfully Started!!!'
